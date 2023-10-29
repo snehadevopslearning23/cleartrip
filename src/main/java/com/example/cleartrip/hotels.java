81dae483557225e7abb0b6d43f3336088fc138b2
Asit Kumar <asitforlearning@gmail.com>
2023-10-29 06:55:15 +0530

    hello

--- src/main/java/com/example/cleartrip/hotels.java
+++ src/main/java/com/example/cleartrip/hotels.java
@@ -7,5 +7,5 @@ import org.springframework.web.bind.annotation.RestController;
 public class hotels {
     @GetMapping("/hotels")
 
-    public String getData() {return "Please book your hotels for ahmedabad" ; }
+    public String getData() {return "Please book your hotels for 1" ; }
 }
