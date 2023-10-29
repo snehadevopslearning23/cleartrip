9ed5e0ae81394ad9195d3382387b9b33b12d9160
Asit Kumar <asitforlearning@gmail.com>
2023-10-29 06:00:00 +0530

    added hotels

---
+++ src/main/java/com/example/cleartrip/hotels.java
@@ -0,0 +1,10 @@
+package com.example.cleartrip;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class hotels {
+    @GetMapping("/hotels")
+    public String getData() {return "Please book your hotels for Indigo" ; }
+}
