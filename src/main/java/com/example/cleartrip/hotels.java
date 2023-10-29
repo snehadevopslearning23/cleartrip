7117b17e4b49682282f7392eb5e247090792916c
Asit Kumar <asitforlearning@gmail.com>
2023-10-29 06:39:09 +0530

    changed hotels

--- src/main/java/com/example/cleartrip/hotels.java
+++ src/main/java/com/example/cleartrip/hotels.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class hotels {
     @GetMapping("/hotels")
-    public String getData() {return "Please book your hotels for ginger" ; }
+    public String getData() {return "Please book your hotels for arrr" ; }
 }
